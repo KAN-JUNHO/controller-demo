785034781311962cf5c7643cf3f1840ddac88287
junho <junho6667@gmail.com>
2021-08-04 14:08:46 +0900

    2021/08/04

--- src/main/java/com/cos/controllerdemo/web/JavaToJspController.java
+++ src/main/java/com/cos/controllerdemo/web/JavaToJspController.java
@@ -1,7 +1,10 @@
 package com.cos.controllerdemo.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.cos.controllerdemo.domain.User;
 
 @Controller
 public class JavaToJspController {
@@ -10,5 +13,12 @@ public class JavaToJspController {
     public String jspToJava(){
         return "d";
     }
-
+    
+    @GetMapping("/jsp/java/model")
+    public String jspToJavaMdoel(Model model) {
+    	User user = new User();
+    	user.setUsername("ssar");
+    	model.addAttribute("username",user.getUsername());
+    	return "e";
+    }
 }
